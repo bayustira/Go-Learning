507940e02d5559f272cae2bb128cf3819da1831f
bayustira <bayustiraputra@gmail.com>
2025-12-25 10:41:43 +0800

    Added calculateFutureValues function to compute future investment values.

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
 	var investmentAmount float64
 	var expectedReturnRate float64
 	var years float64
@@ -18,8 +19,9 @@ func main() {
 	outputText("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	formattedFV := fmt.Sprintf("Future Value: %.2f\n", futureValue)
 	formattedFRV := fmt.Sprintf("Future Value (adjusted for Inflation): %.2f\n", futureRealValue)
@@ -32,4 +34,10 @@ func main() {
 
 func outputText(text string) {
 	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (futureValue float64, futureRealValue float64) {
+	futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue = futureValue / math.Pow(1+inflationRate/100, years)
+	return futureValue, futureRealValue
 }
