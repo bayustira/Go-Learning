d4fe55c5479a818199b504054b9696de59ea7bab
bayustira <bayustiraputra@gmail.com>
2025-12-26 10:23:16 +0800

    Added custom functions for user input & profit calculation

--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -3,22 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	revenue := getUserInput("Revenue:")
+	expenses := getUserInput("Expenses:")
+	taxRate := getUserInput("Tax Rate:")
 
-	fmt.Print("Revenue:")
-	fmt.Scan(&revenue)
-	fmt.Print("Expenses:")
-	fmt.Scan(&expenses)
-	fmt.Print("Tax Rate:")
-	fmt.Scan(&taxRate)
+	earningBeforeTax, earningAfterTax, ratio := calculateProfit(revenue, expenses, taxRate)
 
+	fmt.Printf("EBT: %.2f\n", earningBeforeTax)
+	fmt.Printf("Profit: %.2f\n", earningAfterTax)
+	fmt.Printf("Ratio: %.2f\n", ratio)
+}
+
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculateProfit(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	earningBeforeTax := revenue - expenses
 	earningAfterTax := earningBeforeTax * (1 - taxRate/100)
 	ratio := earningBeforeTax / earningAfterTax
-
-	fmt.Println("EBT:", earningBeforeTax)
-	fmt.Println("Profit:", earningAfterTax)
-	fmt.Println("Ratio:", ratio)
+	return earningBeforeTax, earningAfterTax, ratio
 }
