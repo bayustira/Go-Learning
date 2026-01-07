467ec4b0516735ce21b67240fafa5266ceffc505
bayustira <bayustiraputra@gmail.com>
2026-01-07 17:04:01 +0800

    Create presentMenu function in communication.go and call it in bank.go main loop.

---
+++ bank_application/communication.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+func presentMenu() {
+	fmt.Println("What would you like to do today?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit funds")
+	fmt.Println("3. Withdraw funds")
+	fmt.Println("4. Exit")
+}
+		
