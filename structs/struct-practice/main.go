92e9161777a17dc41fd7e4f855cc4ac83f6dbb58
bayustira <bayustiraputra@gmail.com>
2026-04-13 10:00:08 +0800

    Add Type Switches to printSomething function

--- structs/struct-practice/main.go
+++ structs/struct-practice/main.go
@@ -20,8 +20,13 @@ type outputtable interface {
 }
 
 func main() {
+	printSomething(1)
+	printSomething(3.14)
+	printSomething("Hello, World!")
+
 	title, content := getNoteData()
 	todoText := getUserInput("Todo text:")
+
 
 	todo, err := todo.New(todoText)
 	if err != nil {
@@ -42,7 +47,14 @@ func main() {
 }
 
 func printSomething(value any) {
-	fmt.Println(value)
+	switch value.(type) {
+	case int:
+		fmt.Println("Integer:", value)
+	case float64:
+		fmt.Println("Float:", value)
+	case string:
+		fmt.Println(value)
+	}
 }
 
 func outputData(data outputtable) error {
